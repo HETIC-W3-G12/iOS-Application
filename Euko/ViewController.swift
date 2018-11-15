fa48bc30e83498be769096bcfc41b5917fd35910
victor lucas <victor.lucas@hetic.net>
2018-11-15 11:42:35 +0100

    Adding the base project (Empty files)

---
+++ Euko/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Euko
+//
+//  Created by Victor Lucas on 13/11/2018.
+//  Copyright © 2018 Victor Lucas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
