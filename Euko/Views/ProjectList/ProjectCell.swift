ae36fd03187280cff38d4ccef1963450939b2a02
victor lucas <victor.lucas@hetic.net>
2018-12-14 14:01:26 +0100

    Adding the View Controller and started to custom it

---
+++ Euko/Views/ProjectList/ProjectCell.swift
@@ -0,0 +1,26 @@
+//
+//  ProjectCell.swift
+//  Euko
+//
+//  Created by Victor Lucas on 14/12/2018.
+//  Copyright © 2018 Victor Lucas. All rights reserved.
+//
+
+import UIKit
+
+class ProjectCell: UITableViewCell {
+
+    @IBOutlet weak var insideView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Euko/Views/ProjectList/ProjectListVC.swift
@@ -0,0 +1,20 @@
+//
+//  ProjectListVC.swift
+//  Euko
+//
+//  Created by Victor Lucas on 14/12/2018.
+//  Copyright © 2018 Victor Lucas. All rights reserved.
+//
+
+import UIKit
+
+class ProjectListVC: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
