1bdb9ab119689692559a54b161f8f37aa7efd97c
victor lucas <victor.lucas@hetic.net>
2018-12-15 01:54:10 +0100

    Added dynamic height on project descriptions

--- Euko/Views/Project/ProjectVC.swift
+++ Euko/Views/Project/ProjectVC.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ProjectVC: UIViewController {
 
+    @IBOutlet weak var textViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var shadowButton: UIView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionTextView: UITextView!
     @IBOutlet weak var firstInformationLabel: UILabel!
@@ -23,6 +25,13 @@ class ProjectVC: UIViewController {
         super.viewDidLoad()
         
         self.payButton.layer.cornerRadius = self.payButton.frame.height / 2
+        self.shadowButton.layer.cornerRadius = self.shadowButton.frame.height / 2
+        self.shadowButton.layer.shadowColor = UIColor.black.cgColor
+        self.shadowButton.layer.shadowOpacity = 0.7
+        self.shadowButton.layer.shadowOffset = CGSize(width: -3, height: 3)
+        self.shadowButton.layer.shadowRadius = 1
+        
+        self.descriptionTextView.layer.cornerRadius = 5
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -37,7 +46,15 @@ class ProjectVC: UIViewController {
         
         let interests:Float = self.project.interests * 100
         let margin:Float = self.project.finalPrice - Float(self.project.price)
-        self.secondInformationLabel.text = "\(interests)% intérêts | \(margin)€ de bénéfices"
+        self.secondInformationLabel.text = String(format: "%.2f%% intérêts | %.2f€ de bénéfices", interests,  margin)
+        
+        if (self.descriptionTextView.contentSize.height > 250) {
+            self.textViewHeightConstraint.constant = 250
+            self.descriptionTextView.isScrollEnabled = true
+        } else {
+            self.textViewHeightConstraint.constant = self.descriptionTextView.contentSize.height + 26
+            self.descriptionTextView.isScrollEnabled = false
+        }
     }
     
     @IBAction func payAction(_ sender: Any) {
