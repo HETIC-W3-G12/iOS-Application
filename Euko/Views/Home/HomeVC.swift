0ae2643bfcb0a5090f7bcedbad35680aa24ef602
victor lucas <victor.lucas@hetic.net>
2018-12-22 10:20:21 +0100

    Added the MoneyBack Screen

--- Euko/Views/Home/HomeVC.swift
+++ Euko/Views/Home/HomeVC.swift
@@ -12,11 +12,15 @@ class HomeVC: UIViewController {
 
     @IBOutlet weak var createProjectButton: UIButton!
     @IBOutlet weak var shadowButton: UIView!
+    @IBOutlet weak var moneyBackButton: UIButton!
+    @IBOutlet weak var shadow2Button: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.createProjectButton.layer.cornerRadius = self.createProjectButton.frame.height / 2
+        
+        self.moneyBackButton.layer.cornerRadius = self.moneyBackButton.frame.height / 2
         
         self.shadowButton.layer.cornerRadius = self.shadowButton.frame.height / 2
         self.shadowButton.layer.shadowColor = UIColor.black.cgColor
@@ -24,6 +28,17 @@ class HomeVC: UIViewController {
         self.shadowButton.layer.shadowOffset = CGSize(width: -3, height: 3)
         self.shadowButton.layer.shadowRadius = 1
 
+        self.shadow2Button.layer.cornerRadius = self.shadow2Button.frame.height / 2
+        self.shadow2Button.layer.shadowColor = UIColor.black.cgColor
+        self.shadow2Button.layer.shadowOpacity = 0.7
+        self.shadow2Button.layer.shadowOffset = CGSize(width: -3, height: 3)
+        self.shadow2Button.layer.shadowRadius = 1
+
+    }
+    
+    @IBAction func moneyBackAction(_ sender: Any) {
+        let vc = self.storyboard?.instantiateViewController(withIdentifier: "MoneyBackVC") as! MoneyBackVC
+        self.navigationController?.pushViewController(vc, animated: true)
     }
     
     @IBAction func createProject(_ sender: Any) {
