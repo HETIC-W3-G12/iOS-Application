2404f67cea7fb74dd168eefbd82a38ca42c5e075
victor lucas <victor.lucas@hetic.net>
2019-01-29 09:03:30 +0100

    Added a shadow on the "finance project" button in ProjectVC

--- Euko/Views/Project/ProjectVC.swift
+++ Euko/Views/Project/ProjectVC.swift
@@ -11,13 +11,13 @@ import UIKit
 class ProjectVC: UIViewController {
 
     @IBOutlet weak var textViewHeightConstraint: NSLayoutConstraint!
-    @IBOutlet weak var shadowButton: UIView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionTextView: UITextView!
     @IBOutlet weak var firstInformationLabel: UILabel!
     @IBOutlet weak var secondInformationLabel: UILabel!
     @IBOutlet weak var payButton: UIButton!
-  
+    @IBOutlet weak var buttonShadowView: UIView!
+    
     var project:Project!
     
     @IBAction func payAction(_ sender: Any) {
@@ -43,24 +43,22 @@ extension ProjectVC {
 // MARK: other functions
 extension ProjectVC{
     func setupView(){
-        self.descriptionTextView.roundBorder(radius: 5)
-        self.shadowButton.roundBorder()
-        self.payButton.roundBorder()
+        self.buttonShadowView.setSpecificShadow()
         
-        self.shadowButton.setSpecificShadow()
+        self.descriptionTextView.roundBorder(radius: 5)
+        self.buttonShadowView.roundBorder()
+        self.payButton.roundBorder()
     }
     
     func setupField(){
         self.titleLabel.text = self.project.title
         self.descriptionTextView.text = self.project.description
         
-        let price:Int = self.project.price
-        let timeLaps:Int = self.project.timeLaps
-        self.firstInformationLabel.text = "\(price)€ sur \(timeLaps) mois"
-        
         let interests:Float = self.project.interests * 100
+        self.firstInformationLabel.text = String(format: "%.2f%%", interests)
+        
         let margin:Float = self.project.finalPrice - Float(self.project.price)
-        self.secondInformationLabel.text = String(format: "%.2f%% intérêts | %.2f€ de bénéfices", interests,  margin)
+        self.secondInformationLabel.text = String(format: "%.2f€",  margin)
         
         if (self.descriptionTextView.contentSize.height > 250) {
             self.textViewHeightConstraint.constant = 250
